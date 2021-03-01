145851d272d0912e4a1ca62058e5fcd23a27cb00
rmatsuda <reo.matsuda@gmail.com>
2021-02-28 19:14:53 -0600

    Fixed application.rb

--- app/models/referral.rb
+++ app/models/referral.rb
@@ -1,4 +1,4 @@
 class Referral < ApplicationRecord
-    belongs_to: :member, class_name: Member
-    belongs_to: :attendee, class_name: Attendee
+    belongs_to :member, class_name: "Member"
+    belongs_to :attendee, class_name: "Attendee"
 end
