f7fee967744bd020f0a3cd3f21840e11e2667976
nick5616 <nickbelovoskey@gmail.com>
2021-03-14 09:37:27 -0400

    made home page more responsive and mobile friendly

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -5,27 +5,29 @@ import LinkButton from "./common/LinkButton";
 import EventsView from "./EventsView";
 
 export default () => (
-  <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
-    <div className="jumbotron jumbotron-fluid bg-transparent">
-      <div className="container secondary-color">
-        <h1 className="display-4">MSC Fish - Program Assistant</h1>
-        <p className="lead">
-          <div class="row">
-            <div class="column">
-              <AnnouncementsView></AnnouncementsView>
-            </div>
-            <div class="column">
-              <EventsView></EventsView>
-            </div>
-          </div>
-            
-          
-        </p>
-        <hr className="my-4" />
-        <LinkButton to = "/events" text = "Events"></LinkButton>
-        <LinkButton to = "/announcements" text = "Announcements"></LinkButton>
-        <LinkButton to = "/CheckIn" text = "Check In"></LinkButton>
+  
+    
+      
+      <div className="center-desktop">
+
+        
+        <div>
+          <h1>MSCFISH <i>Program Assistant</i></h1>
+        </div>
+       
+        <div className="left">
+          <AnnouncementsView></AnnouncementsView>
+        </div>
+ 
+        <div className="right">
+          <EventsView></EventsView>
+        </div>
+       
+        <LinkButton className =  "to-button" to = "/events" text = "Events"></LinkButton>
+        <LinkButton className =  "to-button" to = "/announcements" text = "Announcements"></LinkButton>
+        <LinkButton className =  "to-button" to = "/CheckIn" text = "Check In"></LinkButton>
+       
       </div>
-    </div>
-  </div>
+   
+  
 );
