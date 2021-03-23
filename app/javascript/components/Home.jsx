56aa05376b50363d47d2c0fb234dd5ead50f90d8
nick5616 <nickbelovoskey@gmail.com>
2021-03-22 23:00:57 -0400

    Added navbar

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -6,12 +6,21 @@ import LatestEventsView from "./LatestEventsView";
 
 export default () => (
   
-    
+
       <div style = {{width: "100vw", height: "100vh", backgroundColor: "whitesmoke"}}>
+        
+        <div class="sidebar">
+          <img src = "logo.jpeg" alt = "MSC FISH logo"></img>
+          <Link className =  "custom-button lg" to = "/allevents">Events</Link>
+          <Link className =  "custom-button lg" to = "/announcements">Announcements</Link>
+          <Link className =  "custom-button lg" to = "/checkin/1">Check In</Link>
+          <a href="index/database_dump.zip" className =  "custom-button lg" download = "database_dump.zip">
+              Download Database
+          </a>
+        </div>
+        
         <div className = "py-5">
           <div className="center-desktop">
-
-            
             <div>
               <h1 className = "heading">MSCFISH <i>Program Assistant</i></h1>
             </div>
@@ -26,14 +35,11 @@ export default () => (
             
             <LinkButton className =  "to-button" to = "/allevents" text = "Events"></LinkButton>
             <LinkButton className =  "to-button" to = "/announcements" text = "Announcements"></LinkButton>
-            <LinkButton className =  "to-button" to = "/CheckIn" text = "Check In"></LinkButton>
             <LinkButton className =  "to-button" to = "/checkin/1" text = "Check In"></LinkButton>
-          <a href="index/database_dump.zip" download = "database_dump.zip">
-            <button className = "btn btn-lg custom-button">Download Database</button>
-          </a>
-  
-        </div>
-       
+            <a href="index/database_dump.zip" download = "database_dump.zip">
+              <button className = "btn btn-lg custom-button">Download Database</button>
+            </a>
+        </div>       
       </div>
     </div>
 );
