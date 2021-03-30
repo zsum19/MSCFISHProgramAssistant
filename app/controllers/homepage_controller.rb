850440ed62ed26c1952b7761d282c4fc185e6210
treydinges <treydinges@gmail.com>
2021-03-30 16:02:31 -0500

    moved rubyzip gem to all environments

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -18,24 +18,27 @@ class HomepageController < ApplicationController
         filenames = ['announcements.csv', 'events.csv', 'attendees.csv', 'members.csv',
                      'roles.csv', 'eventattendances.csv', 'referrals.csv']
 
-        zipname = 'public/database_dump.zip'
+        current_directory = Dir.pwd
+        zipname = current_directory + '/public/downloads/database_dump.zip'
         File.delete(zipname) if File.exist?(zipname)
 
         Zip::File.open(zipname, Zip::File::CREATE) do |zipfile|
           objects.count.times do |i|
-            file = File.open(filenames[i], 'w')
-            File.write(filenames[i], objects[i].to_csv)
+            file = File.open('public' + filenames[i], 'w')
+            File.write('public' + filenames[i], objects[i].to_csv)
             file.close
-            zipfile.add(filenames[i], file)
+            zipfile.add('public' + filenames[i], file)
           end
         end
 
-        File.open(zipname, 'r') do |f|
-          send_data f.read
-        end
+        # File.open(zipname, 'r') do |f|
+        #   send_data f.read
+        # end
+
+        send_file zipname
 
         filenames.each do |filename|
-          File.delete(filename)
+          File.delete('public' + filename)
         end
       end
     end
