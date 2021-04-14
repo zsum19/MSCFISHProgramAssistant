465dacdab9d32304dc873673c1dc8a3d7297f2c5
treydinges <treydinges@gmail.com>
2021-04-14 00:04:55 -0500

    split csv by \r\n instead of just \n

--- app/javascript/components/adminComponents/MemberList.jsx
+++ app/javascript/components/adminComponents/MemberList.jsx
@@ -17,7 +17,8 @@ class MemberList extends React.Component {
           var result = [];
           var headers = lines[0].split(",");
           for (var i = 0; i < headers.length; i++) {
-            var stripped_word = current_line[j].replace(/\r?\n|\r/g, "");
+            var stripped_word = current_line[j].replace(/(\r\n|\n|\r)/gm, "");
+            console.log(stripped_word);
             headers[i] = stripped_word;
           }
     
